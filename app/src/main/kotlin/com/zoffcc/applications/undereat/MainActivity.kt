1e0f400d9be05a3ab8a14c59689c46c2dd104def
zoff99 <zoff99@users.noreply.github.com>
2025-05-25 11:57:08 +0200

    layout tweaks

--- app/src/main/kotlin/com/zoffcc/applications/undereat/MainActivity.kt
+++ app/src/main/kotlin/com/zoffcc/applications/undereat/MainActivity.kt
@@ -5,9 +5,11 @@ package com.zoffcc.applications.undereat
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,13 +19,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,7 +65,7 @@ import java.util.Random
 val TAG = "UnderEat"
 
 val messages = MutableStateFlow("running tests ...")
-const val DEBUG_COMPOSE_UI_UPDATES = false // set "false" for release builds
+const val DEBUG_COMPOSE_UI_UPDATES = true // set "false" for release builds
 
 
 class MainActivity : ComponentActivity() {
@@ -178,7 +193,8 @@ fun MainScreen() {
                     itemsIndexed(items = restaurants.restaurantlist,
                         key = { index, item -> item.id }
                     ) { index, data ->
-                        Text(text = data.name + " " + data.address)
+                        RestaurantCard(index, data)
+
                     }
                 }
             }
@@ -208,7 +224,9 @@ fun MainScreen() {
                         )
                     }
                 )
-                Spacer(modifier = Modifier.width(50.dp).weight(10F))
+                Spacer(modifier = Modifier
+                    .width(50.dp)
+                    .weight(10F))
                 Button(
                     modifier = Modifier
                         .height(50.dp)
@@ -227,6 +245,45 @@ fun MainScreen() {
                         )
                     }
                 )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RestaurantCard(index: Int, data: Restaurant) {
+    OutlinedCard(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable {},
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp,
+            bottomEnd = 4.dp,
+            bottomStart = 4.dp,
+        ),
+        elevation =  CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            verticalAlignment = CenterVertically,
+            modifier = Modifier
+                .padding(4.dp)
+        ) {
+            Text(
+                text = data.name + " " + data.address,
+                modifier = Modifier
+                    .padding(3.dp).randomDebugBorder(),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.randomDebugBorder().width(1.dp).weight(10F))
+            IconButton(
+                onClick = {},
+                modifier = Modifier.randomDebugBorder().size(50.dp)
+            ) {
+                Icon(Icons.Default.LocationOn, contentDescription = "Localized description")
             }
         }
     }
