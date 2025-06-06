215d70b292efe2478bc78d2b963d18c5a6b4d3cf
zoff99 <zoff99@users.noreply.github.com>
2025-06-06 22:36:31 +0200

    actually save lat and lon

--- app/src/main/kotlin/com/zoffcc/applications/undereat/Add_form.kt
+++ app/src/main/kotlin/com/zoffcc/applications/undereat/Add_form.kt
@@ -389,6 +389,8 @@ fun add_form(context: Context) {
                             r.active = true
                             r.for_summer = input_for_summer
                             r.category_id = cat_list[cat_itemPosition.value].id
+                            r.lat = geo_coord_string_to_longdb(input_lat.text)
+                            r.lon = geo_coord_string_to_longdb(input_lon.text)
                             val r_id_new: Long = orma.insertIntoRestaurant(r)
                             restore_mainlist_state()
                             globalstore.updateMainscreenState(MAINSCREEN.MAINLIST)
