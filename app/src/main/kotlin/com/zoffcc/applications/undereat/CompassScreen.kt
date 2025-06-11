84f788b7dcfc98a7e6bd08bb10bcc9716a12036e
zoff99 <zoff99@users.noreply.github.com>
2025-06-11 13:57:24 +0200

    show address in compass screen

--- app/src/main/kotlin/com/zoffcc/applications/undereat/CompassScreen.kt
+++ app/src/main/kotlin/com/zoffcc/applications/undereat/CompassScreen.kt
@@ -12,21 +12,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CompassScreen() {
     Column(modifier = Modifier.fillMaxSize().padding(10.dp)) {
         val restraunt_id = globalstore.getRestaurantId()
         val data = restaurantliststore.get(restraunt_id)
-        Spacer(modifier = Modifier.width(1.dp).height(45.dp))
+        Spacer(modifier = Modifier.width(1.dp).height(35.dp))
         Row {
             Spacer(modifier = Modifier.weight(5F).height(1.dp))
             restaurant_name_view(data, false)
             Spacer(modifier = Modifier.weight(5F).height(1.dp))
         }
+
+        Spacer(modifier = Modifier.width(1.dp).height(15.dp))
+        Row {
+            Spacer(modifier = Modifier.weight(5F).height(1.dp))
+            Text(
+                text = data.address,
+                softWrap = true,
+                maxLines = 2,
+                modifier = Modifier
+                    .randomDebugBorder(),
+                textAlign = TextAlign.Start,
+                style = TextStyle(fontSize = 14.sp)
+            )
+            Spacer(modifier = Modifier.weight(5F).height(1.dp))
+        }
+
         Spacer(modifier = Modifier.width(1.dp).height(15.dp))
         Compass(data = data, compact = false, fullsize = true)
     }
